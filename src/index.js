0286b67d8a381ac056145e185c02dd87c28ec843
J-Skarbek <john@snarkmediaagency.com>
2022-10-14 11:29:06 -0500

    Starting the dropdown element.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import './dropdown.js';
 
 function component() {
   const element = document.createElement('div');
@@ -7,5 +8,12 @@ function component() {
   element.classList.add('hello');
   return element;
 }
+
+function dropDownComponent() {
+  const containerDropDown = document.createElement('div');
+  containerDropDown.innerHTML = 'Testing'
+  return containerDropDown;
+}
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
+document.body.appendChild(dropDownComponent());
