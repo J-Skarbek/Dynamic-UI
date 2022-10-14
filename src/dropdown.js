900ecada3664049a73b5712543b8a846d0d63286
J-Skarbek <john@snarkmediaagency.com>
2022-10-14 16:27:51 -0500

    Clean up unnecessary code and finish modulizing the dropdown components.

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,18 +1,29 @@
 function createDropDown() {
-  getDropDownContainer = document.querySelector('.dropdown')
+  const getDropDownContainer = document.querySelector('.dropdown')
   const createBtn = document.createElement('button')
   createBtn.classList.add('dropBtn')
   createBtn.innerText = 'Dropdown'
 
-  const dropDownContent = document.createElement('div').classList.add('dropdown-content')
+  const dropDownContent = document.createElement('div')
+  dropDownContent.classList.add('dropdown-content')
 
   const linkItem = document.createElement('a')
+  linkItem.innerText = 'Link 1'
+  const linkItem2 = document.createElement('a')
+  linkItem2.innerText = 'Link 2'
+  const linkItem3 = document.createElement('a')
+  linkItem3.innerText = 'Link 3'
 
-  
+  getDropDownContainer.appendChild(createBtn)
+  getDropDownContainer.appendChild(dropDownContent)
+  dropDownContent.appendChild(linkItem)
+  dropDownContent.appendChild(linkItem2)
+  dropDownContent.appendChild(linkItem3)
+  return getDropDownContainer
 }
 
 const appendDropDown = () => {
-  const dropDownButton = document.querySelector('.dropbtn');
+  const dropDownButton = document.querySelector('.dropBtn');
   dropDownButton.addEventListener('click', activateDropDown)
 }
 
@@ -20,4 +31,4 @@ function activateDropDown() {
   console.log('testing dd button')
 }
 
-export { appendDropDown, activateDropDown }
+export { createDropDown, appendDropDown, activateDropDown }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import _ from 'lodash';
 import './style.css';
-import { appendDropDown } from './dropdown';
-// import './dropdown.js';
+import { appendDropDown, createDropDown } from './dropdown';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
-}
-
-function dropDownComponent() {
-  const containerDropDown = document.createElement('div');
-  containerDropDown.innerHTML = 'Testing'
-  return containerDropDown;
-}
-  
-document.body.appendChild(component());
-document.body.appendChild(dropDownComponent());
+document.body.appendChild(createDropDown());
 appendDropDown();
