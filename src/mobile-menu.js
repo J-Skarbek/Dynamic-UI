6a25946277c973af5a00474620db15d08d78e679
J-Skarbek <john@snarkmediaagency.com>
2022-10-19 16:30:53 -0500

    Start on the mobile menu creation.

---
+++ src/mobile-menu.js
@@ -0,0 +1,12 @@
+import AddCircle from './icons/add_circle_FILL0_wght100_GRAD200_opsz40.svg';
+import AddFillCircle from './icons/add_circle_FILL1_wght100_GRAD200_opsz40.svg';
+import AddCancelCirlce from './icons/cancel_FILL0_wght100_GRAD200_opsz40.svg';
+import AddFillCancelCircle from './icons/cancel_FILL1_wght100_GRAD200_opsz40.svg';
+import AddFavorite from './icons/favorite_FILL0_wght100_GRAD200_opsz40.svg';
+import AddFillFavorite from './icons/favorite_FILL1_wght100_GRAD200_opsz40.svg';
+import AddHome from './icons/home_FILL0_wght100_GRAD200_opsz40.svg';
+import AddFillHome from './icons/home_FILL1_wght100_GRAD200_opsz40.svg';
+import AddSearch from './icons/search_FILL0_wght100_GRAD200_opsz40.svg';
+import AddFillSearch from './icons/search_FILL1_wght100_GRAD200_opsz40.svg';
+
+
